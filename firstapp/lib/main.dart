206954aa54e3d958403fabbaf59cfc404e39e255
dinesh-nanditech <dinesh.nanditech@gmail.com>
2023-06-15 09:01:04 +0545

    Column, Icon, Button

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "First Flutter App",
       home: HomePage(),
     ),
@@ -16,15 +17,30 @@ class HomePage extends StatelessWidget {
   @override
   // The build method is called by the Flutter framework whenever the widget needs to be rendered or updated.
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Hello World!!",
-          style: TextStyle(
-            color: Colors.teal,
-            fontSize: 30.0,
-            fontWeight: FontWeight.w600,
-          ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        // use Column to display multiple widgets in a vertical direction
+        body: Column(
+          children: [
+            Text(
+              "Hello World!!",
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Icon(Icons.info_outline, size: 40, color: Colors.red),
+            ElevatedButton(
+              onPressed: () {
+                print("Button Pressed");
+              },
+              child: Text("Press Me"),
+            ),
+          ],
         ),
       ),
     );
